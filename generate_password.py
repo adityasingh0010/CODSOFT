7c561e36c999702527b93b35b29ab148e1114996
adityasingh0010 <145257521+adityasingh0010@users.noreply.github.com>
2023-10-06 12:39:46 +0530

    Add files via upload

---
+++ generate_password.py
@@ -0,0 +1,54 @@
+import random
+import string
+
+class PasswordGenerator:
+    def __init__(self):
+        self.character_sets = {
+            "lowercase": string.ascii_lowercase,
+            "uppercase": string.ascii_uppercase,
+            "digits": string.digits,
+            "special": string.punctuation
+        }
+
+    def generate_password(self, length, include=None, exclude=None, min_strength=3):
+        if length < 4:
+            raise ValueError("Password length should be at least 4 characters.")
+
+        if include is None:
+            include = list(self.character_sets.keys())
+
+        character_set = ''.join([self.character_sets[char] for char in include if char in self.character_sets])
+
+        if exclude is not None:
+            character_set = ''.join([char for char in character_set if char not in exclude])
+
+        if len(character_set) < min_strength:
+            raise ValueError("Password strength requirement not met with the selected character set.")
+
+        password = ''.join(random.choice(character_set) for _ in range(length))
+        return password
+
+def main():
+    password_generator = PasswordGenerator()
+
+    while True:
+        try:
+            length = int(input("Enter the desired password length (at least 4): "))
+            if length < 4:
+                print("Password length should be at least 4 characters. Please try again.")
+                continue
+
+            include = input("Enter character types to include (lowercase, uppercase, digits, special), separated by commas (default: all): ").split(',')
+            exclude = input("Enter characters to exclude (e.g., @#$%): ")
+
+            min_strength = int(input("Enter minimum password strength (number of character types): "))
+
+            password = password_generator.generate_password(length, include=include, exclude=exclude, min_strength=min_strength)
+
+            print("Generated Password:", password)
+            break
+        except ValueError as e:
+            print(e)
+
+if __name__ == "__main__":
+    main()
